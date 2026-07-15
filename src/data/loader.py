92c362314bf8fb56a98c4d422b21732df118cab7
ojhankit <ojha27ankit@gmail.com>
2026-07-15 13:23:05 +0530

    replaced datasetdict to dataset

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from datasets import load_dataset, DatasetDict, load_from_disk
+from datasets import load_dataset, Dataset, load_from_disk
 
 from src.utils import setup_logger
 
@@ -9,7 +9,7 @@ def download_dataset(
     dataset_name: str,
     cache_dir: Path,
     subset_size: int | None = None,
-) -> DatasetDict:
+) -> Dataset:
     """
     Download a dataset from Hugging Face and optionally keep only
     a subset of the training split.
@@ -20,15 +20,15 @@ def download_dataset(
     dataset = load_dataset(
         dataset_name,
         cache_dir=str(cache_dir),
-    )
+    )["train"]
 
     logger.info("Dataset downloaded successfully.")
 
     if subset_size is not None:
         logger.info("Selecting first %d training samples.", subset_size)
 
-        dataset["train"] = dataset["train"].select(
-            range(min(subset_size, len(dataset["train"])))
+        dataset["train"] = dataset.select(
+            range(min(subset_size, len(dataset)))
         )
 
     logger.info("Training samples: %d", len(dataset["train"]))
@@ -36,7 +36,7 @@ def download_dataset(
     return dataset
 
 
-def save_dataset(dataset: DatasetDict, path: Path) -> None:
+def save_dataset(dataset: Dataset, path: Path) -> None:
     """
     Save dataset locally.
     """
@@ -50,7 +50,7 @@ def save_dataset(dataset: DatasetDict, path: Path) -> None:
     logger.info("Dataset saved successfully.")
 
 
-def load_local_dataset(path: Path) -> DatasetDict:
+def load_local_dataset(path: Path) -> Dataset:
     """
     Load dataset from disk.
     """
